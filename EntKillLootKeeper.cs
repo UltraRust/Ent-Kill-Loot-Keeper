be8a7706b7ab6db25e133319efe8ec5fae83b191
UltraRust <53350204+UltraRust@users.noreply.github.com>
2019-08-16 22:32:03 +0200

    LINQ removed for better performance

--- EntKillLootKeeper.cs
+++ EntKillLootKeeper.cs
@@ -13,33 +13,37 @@ namespace Oxide.Plugins
         {
             if (arg.Args != null && arg.Args.Length == 2 && arg.Args[0] == "kill")
             {
-                uint id = Convert.ToUInt32(arg.Args[1]);
-                BaseNetworkable baseNetworkable = BaseNetworkable.serverEntities.Where(w => w.net.ID == id).SingleOrDefault();
-                if (baseNetworkable != null)
+                uint itemId = 0;
+
+                if (uint.TryParse(arg.Args[1], out itemId))
                 {
-                    var container = baseNetworkable as LootContainer;
-                    if (container != null)
+                    BaseNetworkable baseNetworkable = BaseNetworkable.serverEntities.Find(itemId);
+                    if (baseNetworkable != null)
                     {
-                        Vector3 dropPosition = container.GetDropPosition();
-                        Vector3 dropVelocity = container.GetDropVelocity();
-                        float move = 0;
-                        foreach (Item item in container.inventory.itemList)
+                        var container = baseNetworkable as LootContainer;
+                        if (container != null)
                         {
-                            move = move + 0.1F;
-                            dropPosition = new Vector3(dropPosition.x + move, dropPosition.y + 1, dropPosition.z + move);
-
-                            Item itemToDrop = null;
-                            if (item.info.shortname == "blueprintbase")
+                            Vector3 dropPosition = container.GetDropPosition();
+                            Vector3 dropVelocity = container.GetDropVelocity();
+                            float move = 0;
+                            foreach (Item item in container.inventory.itemList)
                             {
-                                itemToDrop = ItemManager.Create(ItemManager.FindItemDefinition("blueprintbase"), 1);
-                                itemToDrop.blueprintTarget = item.blueprintTarget;
-                            }
-                            else
-                            {
-                                itemToDrop = ItemManager.CreateByItemID(item.info.itemid, item.amount);
-                            }
+                                move = move + 0.1F;
+                                dropPosition = new Vector3(dropPosition.x + move, dropPosition.y + 1, dropPosition.z + move);
 
-                            itemToDrop.Drop(dropPosition, dropVelocity, Quaternion.identity);
+                                Item itemToDrop = null;
+                                if (item.info.shortname == "blueprintbase")
+                                {
+                                    itemToDrop = ItemManager.Create(ItemManager.FindItemDefinition("blueprintbase"), 1);
+                                    itemToDrop.blueprintTarget = item.blueprintTarget;
+                                }
+                                else
+                                {
+                                    itemToDrop = ItemManager.CreateByItemID(item.info.itemid, item.amount);
+                                }
+
+                                itemToDrop.Drop(dropPosition, dropVelocity, Quaternion.identity);
+                            }
                         }
                     }
                 }
