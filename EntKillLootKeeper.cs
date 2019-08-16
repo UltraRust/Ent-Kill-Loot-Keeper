45f2a858bcbb274632ab23de0bb9140b73835d89
UltraRust <53350204+UltraRust@users.noreply.github.com>
2019-08-16 22:12:11 +0200

    Add files via upload

---
+++ EntKillLootKeeper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("EntKillLootKeeper", "Ultra", "1.0.1")]
+    [Description("Keeps loot from destroyed container by command ent kill")]
+
+    class EntKillLootKeeper : CovalencePlugin
+    {
+        object OnServerCommand(ConsoleSystem.Arg arg)
+        {
+            if (arg.Args != null && arg.Args.Length == 2 && arg.Args[0] == "kill")
+            {
+                uint id = Convert.ToUInt32(arg.Args[1]);
+                BaseNetworkable baseNetworkable = BaseNetworkable.serverEntities.Where(w => w.net.ID == id).SingleOrDefault();
+                if (baseNetworkable != null)
+                {
+                    var container = baseNetworkable as LootContainer;
+                    if (container != null)
+                    {
+                        Vector3 dropPosition = container.GetDropPosition();
+                        Vector3 dropVelocity = container.GetDropVelocity();
+                        float move = 0;
+                        foreach (Item item in container.inventory.itemList)
+                        {
+                            move = move + 0.1F;
+                            dropPosition = new Vector3(dropPosition.x + move, dropPosition.y + 1, dropPosition.z + move);
+
+                            Item itemToDrop = null;
+                            if (item.info.shortname == "blueprintbase")
+                            {
+                                itemToDrop = ItemManager.Create(ItemManager.FindItemDefinition("blueprintbase"), 1);
+                                itemToDrop.blueprintTarget = item.blueprintTarget;
+                            }
+                            else
+                            {
+                                itemToDrop = ItemManager.CreateByItemID(item.info.itemid, item.amount);
+                            }
+
+                            itemToDrop.Drop(dropPosition, dropVelocity, Quaternion.identity);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
